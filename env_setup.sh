fd7b8a2d6c88339264c35a749f86e08e348a0bda
anielmod <higherlvltuner@aim.com>
2016-08-13 13:02:06 -0400

    updated deconfig

--- env_setup.sh
+++ env_setup.sh
@@ -81,7 +81,7 @@ export TMPFILE=`mktemp -t`;
 
 
 # system compiler
-export CROSS_COMPILE=/home/anielmod/aarch64-linux-gnu-5.3/bin/aarch64-
+export CROSS_COMPILE=/home/anielmod/aarch64-linux-android-5.3-kernel/bin/aarch64-linux-android-
 
 
 # CPU Core
